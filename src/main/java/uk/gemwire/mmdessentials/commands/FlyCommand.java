eb5cb548ab4fb5c55eba629b547d5a1b26f664d6
KiriCattus <kiri_cattus@icloud.com>
2022-09-24 00:20:01 +0100

    Make the fly command actually work! :D

--- src/main/java/uk/gemwire/mmdessentials/commands/FlyCommand.java
+++ src/main/java/uk/gemwire/mmdessentials/commands/FlyCommand.java
@@ -4,17 +4,22 @@ import com.mojang.brigadier.Command;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.Component;
 
+/**
+ * Allow the player to fly creative style when used.
+ */
 public class FlyCommand {
 
     public static int execute(CommandSourceStack player) {
-        //TODO Figure out why it doesn't let the user fly yet changes the values.
-        if (!player.getPlayer().getAbilities().mayfly) {
-            player.getPlayer().getAbilities().mayfly = true;
-            player.getPlayer().sendSystemMessage(Component.translatable("mmdessentials.fly.enabled"));
-        } else {
-            player.getPlayer().getAbilities().mayfly = false;
-            player.getPlayer().getAbilities().flying = false;
-            player.getPlayer().sendSystemMessage(Component.translatable("mmdessentials.fly.disabled"));
+        if (player.getPlayer() != null) {
+            if (!player.getPlayer().getAbilities().mayfly) {
+                player.getPlayer().getAbilities().mayfly = true;
+                player.getPlayer().onUpdateAbilities();
+                player.getPlayer().sendSystemMessage(Component.translatable("Flying enabled!"));
+            } else {
+                player.getPlayer().getAbilities().mayfly = false;
+                player.getPlayer().onUpdateAbilities();
+                player.getPlayer().sendSystemMessage(Component.translatable("Flying disabled!"));
+            }
         }
         return Command.SINGLE_SUCCESS;
     }
