da91291c8b916104cdd3d9e59791e0c3fc565b4f
Curle <curle@gemwire.uk>
2023-03-14 18:01:09 +0000

    Fix TPA feature.

--- src/main/java/uk/gemwire/bareessentials/commands/CmdTeleportRequest.java
+++ src/main/java/uk/gemwire/bareessentials/commands/CmdTeleportRequest.java
@@ -7,61 +7,81 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import uk.gemwire.bareessentials.data.PendingTeleports;
 
 public class CmdTeleportRequest {
+
+    private static Logger logger = LogManager.getLogger();
 
     public static int tpa(CommandContext<CommandSourceStack> pSource) throws CommandSyntaxException {
         var target = EntityArgument.getPlayer(pSource, "user");
         var sender = pSource.getSource().getPlayer();
 
+        logger.info("New Teleport Request; {} wants to teleport to {}.", sender.getDisplayName().getString(), target.getDisplayName().getString());
+
         if (PendingTeleports.getRequestFrom(sender) != null) {
+            logger.info("Player {} already has a teleport request, not considering..", sender.getDisplayName().getString());
             sender.sendSystemMessage(Component.translatable(Language.getInstance()
                 .getOrDefault("bareessentials.tpa.toomanyrequests")));
             return 0;
         }
 
+        logger.info("Request valid, saving..");
         target.sendSystemMessage(Component.translatable(Language.getInstance()
-            .getOrDefault("bareessentials.tpa.incoming"), sender.getDisplayName()));
-        PendingTeleports.PENDING.add(new PendingTeleports.TeleportRequest(target, sender, true));
+            .getOrDefault("bareessentials.tpa.incoming"), sender.getDisplayName().getString()));
+
+        PendingTeleports.PENDING.add(new PendingTeleports.TeleportRequest(sender, target, true));
+
         sender.sendSystemMessage(Component.translatable(Language.getInstance()
-            .getOrDefault("bareessentials.tpa.sent"), target.getDisplayName()));
+            .getOrDefault("bareessentials.tpa.sent"), target.getDisplayName().getString()));
 
         return Command.SINGLE_SUCCESS;
     }
 
     public static int accept(CommandContext<CommandSourceStack> pSource) {
         var target = pSource.getSource().getPlayer();
+        logger.info("Player {} is accepting a pending request..", target.getDisplayName().getString());
+
         var request = PendingTeleports.getRequestFor(target);
 
         if (request == null) {
+            logger.info("Player {} has no pending requests to accept.", target.getDisplayName().getString());
             target.sendSystemMessage(Component.translatable(Language.getInstance()
                 .getOrDefault("bareessentials.tpa.norequests")));
             return 0;
         }
 
+        logger.info("Request valid, teleporting {} to {}.", request.sender().getDisplayName().getString(), request.receiver().getDisplayName().getString());
         request.sender().sendSystemMessage(Component.translatable(Language.getInstance()
-            .getOrDefault("bareessentials.tpa.inprogress")));
-        target.sendSystemMessage(Component.translatable(Language.getInstance()
-            .getOrDefault("bareessentials.tpa.inprogress")));
+            .getOrDefault("bareessentials.tpa.inprogress"), request.receiver().getDisplayName().getString()));
+        target.sendSystemMessage(Component.translatable(Language.getInstance() .getOrDefault("bareessentials.tpa.inprogress"),
+            Component.translatable(Language.getInstance().getOrDefault("bareessentials.tpa.targetyou"))
+        ));
         request.sender().teleportTo(target.getX(), target.getY(), target.getZ());
+
+        PendingTeleports.removeRequestFrom(request.sender());
 
         return Command.SINGLE_SUCCESS;
     }
 
     public static int deny(CommandContext<CommandSourceStack> pSource) {
         var target = pSource.getSource().getPlayer();
+        logger.info("Player {} wants to deny a teleport request.", target.getDisplayName().getString());
         var request = PendingTeleports.getRequestFor(target);
 
         if (request == null) {
+            logger.info("Player {} has no pending teleport requests.", target.getDisplayName().getString());
             target.sendSystemMessage(Component.translatable(Language.getInstance()
                 .getOrDefault("bareessentials.tpa.norequests")));
             return 0;
         }
 
-        PendingTeleports.PENDING.remove(request);
+        logger.info("Request valid, removing teleport request from {} to {}.", request.sender().getDisplayName().getString(), request.receiver().getDisplayName().getString());
+        PendingTeleports.removeRequestFrom(request.sender());
         request.sender().sendSystemMessage(Component.translatable(Language.getInstance()
-            .getOrDefault("bareessentials.tpa.denied"), target.getDisplayName()));
+            .getOrDefault("bareessentials.tpa.denied"), target.getDisplayName().getString()));
         target.sendSystemMessage(Component.translatable("bareessentials.tpa.denysuccess"));
 
         return Command.SINGLE_SUCCESS;
--- src/main/java/uk/gemwire/bareessentials/data/PendingTeleports.java
+++ src/main/java/uk/gemwire/bareessentials/data/PendingTeleports.java
@@ -1,11 +1,14 @@
 package uk.gemwire.bareessentials.data;
 
 import net.minecraft.server.level.ServerPlayer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PendingTeleports {
+    private static Logger logger = LogManager.getLogger();
 
     public record TeleportRequest(ServerPlayer sender, ServerPlayer receiver, boolean pending) {
     }
@@ -23,11 +26,20 @@ public class PendingTeleports {
     }
 
     public static TeleportRequest getRequestFor(ServerPlayer target) {
+        logger.info("Finding a pending request that teleports to {}", target.getDisplayName().getString());
         for (TeleportRequest request : PENDING) {
-            if (request.receiver == target)
+            logger.info("Considering Request from {} to {}.", request.sender.getDisplayName().getString(), request.receiver.getDisplayName().getString());
+            if (request.receiver == target) {
+                logger.info("Request valid, target found.");
                 return request;
+            }
         }
 
         return null;
+    }
+
+    public static void removeRequestFrom(ServerPlayer sender) {
+        logger.info("Removing a teleport request from {}", sender.getDisplayName().getString());
+        PENDING.remove(getRequestFrom(sender));
     }
 }
